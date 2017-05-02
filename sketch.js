885763af84bf53cfeed38b1876483bb63b14719c
Dong-ha Lee <dhl347@nyu.edu>
2017-05-02 15:05:05 -0400

    bunch added

--- sketch.js
+++ sketch.js
@@ -7,9 +7,8 @@ for finals:
 - ice plate shape and opacity
 - water rise as the day goes on and the other way if night
 - time element */
-
-var sea, backgroundImage, sun, moon, day, bear, n, ground;
-var icePlate = [];
+var hole = [];
+var sea, backgroundImage, sun, moon, day, bear, n, ground,hole;
 var sun_orbit_stops_xpos = [];
 var sun_orbit_stops_ypos = [];
 var moon_orbit_stops_xpos = [];
@@ -19,24 +18,30 @@ var y_gridpoint_12scale = [];
 var bearposition_x = []; //top ->bottom left->right / bearposition of leftbottom corner
 var bearposition_y = []; // 0top 1bottom
 var s24;
-var vertical_midpoint;
+var top_boundary;
 var mid_start_x;
 var move = false;
-var seaRise;
+var sea_level=0;
 var plate_boundary;
-var step = 0;
-var bear_length;
 var x_trans, y_trans;
 var position = 1;
 var weatherData,current_weather;  //weather vars
-var bear_pos_x,bear_pos_y
-
+var bear_pos_x,bear_pos_y;
+var timer;
+var sea_level_increment,sea_level_decrement;
+var day_count=0;
+var live;
+var current_sec = 0;
+var fish;
+var eatig;
 
 function preload(){
 	// weather id = https://openweathermap.org/weather-conditions
 	var url = 'http://api.openweathermap.org/data/2.5/weather?q=Churchill&APPID=65f68b9f954837334ed821398fef3b96';
 	weatherData = loadJSON(url);
 }
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255);
@@ -51,16 +56,22 @@ function setup() {
 	moon_orbit_stops_ypos = [height * 3 / 8, -400, -400, height * 3 / 8, height / 8, height / 8];
 	current_weather = weatherData.weather[0].main;
 	bear_pos_x = width/2;
-	bear_pos_y = height/2;
+	bear_pos_y = height/4*3	;
 
 	//set initial setup
 	sea = new Sea();
 	background_image = new Background_image(current_weather);
+
 	//parameters == starting location
 	sun = new Sun(); //creates sun that rotates with 6 stops.
 	moon = new Moon(); //opposite side of sun
 	ground = new Ground();
-
+	n=random(1,4);
+	// for(var i =0;i<=n;i++){
+	// 	hole[i] = new Hole();
+	// }
+	hole = new Hole();
+	fish = new Fish();
 	//build a bear
 	bear = new Polarbear(bear_pos_x,bear_pos_y);
 	//display initial
@@ -69,7 +80,7 @@ function setup() {
 	sea.display();
 	background_image.display(day);
 
-		fill(0);
+	fill(0);
 	textAlign(CENTER);
   textSize(30);
 	text("Use Left and Right arrow to move the bear!",width/2,y_gridpoint_12scale[8]);
@@ -77,22 +88,33 @@ function setup() {
 	sun.display(sun_orbit_stops_xpos[0], sun_orbit_stops_ypos[0]);
 	moon.display(moon_orbit_stops_xpos[0], moon_orbit_stops_ypos[0]);
 	bear.draw(bear_pos_x,bear_pos_y);
-
-	// bear.translate(x_gridpoint_12scale[3],y_gridpoint_12scale[7]);
-
-	// quad(width / 6, height / 2 + height / 12, width * 5 / 6, height / 2 + height / 12, width, height - height / 12, 0, height - height / 12);
-	// stroke(0);
-	// line(x_gridpoint_12scale[1], y_gridpoint_12scale[9], x_gridpoint_12scale[11], y_gridpoint_12scale[9]);
+	setTimer(); //sets a minute timer that toggles day/night
 }
 
 
 function draw() {
  ground.display();
- sea.display();
  background_image.display(day);
+ if(day){
+	 sea.rise();
+	//  ground.update_d();
+	sea.display();
+ }
+ else if(!day){
+	 sea.freeze();
+	//  ground.update_i();
+	sea.display();
+ }
 	//
+// 	for(var i =0;i<hole.length;i++){
+// 	hole[i].display();
+// }
+hole.display();
  sun.display(sun_orbit_stops_xpos[0], sun_orbit_stops_ypos[0]);
  moon.display(moon_orbit_stops_xpos[0], moon_orbit_stops_ypos[0]);
+
+
+ fish.display();
  bear.draw(bear_pos_x,bear_pos_y);
  if(keyIsDown(LEFT_ARROW))
  bear_pos_x-=10;
@@ -104,44 +126,140 @@ function draw() {
  bear_pos_y+=10;
  bear.checkBound();
 
-}
 
-function Ground() {
-	this.display = function() {
-		fill(255);
-		noStroke();
-		rect(0, height / 2, width, height);
+
+ //display day count
+ textSize(32);
+ fill(255);
+ text("Day "+day_count,100,100);
+ top_boundary=sea.level+height/4;
+
+ eating = checkEating(bear_pos_x+40,bear_pos_y+150,fish.x,fish.y);
+ console.log(mouseX+" / "+mouseY);
+ console.log(bear_pos_x+" / "+bear_pos_y+" / "+fish.x+" / "+fish.y+"  / "+eating);
+	if(eating)fish.remove();
+ bear.updateHealth(eating);
+ bear.showHealth();
+ bear.checkDead(top_boundary,hole.x,hole.y,hole.radius,bear.health);
+ checkGameEnd();
+}
+function checkEating(bx,by,fx,fy){
+	if(bx>fx-40&&by<fy-40&&bx>fx+40&&by>fy+40){
+		return true;
+	}
+	else{
+		return false;
 	}
 }
 
+function checkGameEnd(){
+	if(!live){
+		fill(0);
+		rect(0,0,width,height);
+		hole.display();
+		bear.drawDying(bear_pos_x,bear_pos_y);
+		textSize(30);
+		fill(255);
+		text("YOU FEEL ME?", width/2,height/4);
+		text("You survived "+day_count+" days",width/2,height/4+100);
+	}
+}
+
+function setTimer(){
+	timer = setInterval(toggleDay,60000);
+}
+function toggleDay(){
+	if(day) {
+		fish = new Fish();
+		day=false;
+		day_count++;
+		sun.move();
+	}
+	else if(!day)
+	{
+		 fish = new Fish();
+		 day = true;
+		 moon.move();
+	 }
+}
+function Fish(){
+	this.x = random(100,width-100);
+	this.y = random(height/2,height-100);
+	this.color=color(176,175,175);
+	this.width=random(50,100);
+	this.height = random(25,50);
+	this.display = function(){
+		fill(this.color);
+		ellipseMode(CENTER);
+		ellipse(this.x,this.y,this.width,this.height);
+		triangle(this.x+this.width/2-this.x/50,this.y,this.x+this.width/2+this.width/4,this.y+this.height/2,this.x+this.width/2+this.width/4,this.y-this.height/2);
+	}
+	this.remove = function(){
+		this.color = color(0);
+		this.display();
+	}
+}
+function Hole(){
+	this.deep_color = color(8,10,116);
+	this.light_color = color(44,76,144);
+	this.radius = random(width/8,width/4);
+	this.x = (random(this.radius,width-this.radius));
+	this.y = (random(height/2,height-this.radius));
+	this.display = function(){
+
+		ellipseMode(CENTER);
+		 for (var i = 1; i < this.radius; i++) {
+			//this.color=this.deep_color;
+			this.color = lerpColor(this.deep_color,this.light_color,i/this.radius);
+			fill(this.color);
+			noStroke();
+			ellipse(this.x,this.y,this.radius,this.radius/4);
+		}
+	}
+}
+
+
+
+
+function Ground() {
+	this.starting_y = height/2;
+	this.display = function() {
+		fill(255);
+		//noStroke();
+		rect(0, this.starting_y, width, height);
+	}
+	// this.update_i = function(){
+	// 	this.starting_y -= sea_level_increment;
+	// }
+	// this.update_d = function(){
+	//
+	// }
+}
+
+
 function Sea() {
+	var original_level = height/4;
 	this.r = 0;
 	this.g = 105;
 	this.b = 145;
 	this.color = color(this.r, this.g, this.b);
+	this.level = original_level;
 	this.checkColor = function() {
 		var rgbs = this.r + this.g + this.b;
 		return rgbs;
 	}
-	// 	this.generateWaves = function(){
-	// 		var waves = [];
-	// 		for (var i = 1; i < random(5, 20);i++) {
-
-	// 		}
-	// 	}
+	this.rise = function(){
+		sea_level_increment=random(0.01,0.1);
+		this.level +=sea_level_increment ;
+	}
+	this.freeze = function(){
+		sea_level_decrement = random(0.01,0.08);
+		this.level-=sea_level_decrement;
+	}
 	this.display = function() {
 		fill(this.color);
 		noStroke();
-		rect(0, height / 4, width, height / 4);
-	}
-	this.move = function(day, rand) {
-		var increase, decrease;
-		if (day) {
-			rect(0, y_gridpoint_12scale[5], width, rand);
-		}
-		if (!day) {
-
-		}
+		rect(0, height / 4, width, this.level);
 	}
 }
 
@@ -149,23 +267,49 @@ function Background_image(weather) {
 	this.weather = weather;
 	this.daycolor = color(168, 203, 255);
 	this.nightcolor = color(0, 0, 0);
-	this.end = function() {
-		fill(0);
-		rect(0, 0, width, height);
-		fill(255);
-		textSize(40);
-		textAlign(CENTER);
-		text("The LAST POLAR BEAR Died", width / 2, height / 2);
-	}
+
 	this.display = function(day) {
 		//  -TODO- Rain snow sunny cloudy /day &night
 		if (day) {
-			fill(this.daycolor);
+			switch(this.weather){
+				case "Rain":
+					fill(109);
+					break;
+				case "Snow":
+					fill(239,250,255);
+					break;
+				case "Sunny":
+					fill(248,255,132);
+					break;
+				case "Clouds":
+					fill(98,120,141);
+					break;
+				default:
+					fill(248,0,132);
+					break;
+
+			}
 			noStroke();
 			rect(0, 0, width, height / 4);
 
 		} else {
-			fill(this.nightcolor);
+				switch(this.weather){
+					case "Rain":
+						fill(255);
+						break;
+					case "Snow":
+						fill(200);
+						break;
+					case "Sunny":
+						fill(150);
+						break;
+					case "Clouds":
+						fill(50);
+						break;
+					default:
+						fill(0);
+						break;
+				}
 			noStroke();
 			rect(0, 0, width, height / 4);
 		}
@@ -174,46 +318,33 @@ function Background_image(weather) {
 	}
 }
 
+
 function Sun() {
-	this.color = color(253, 184, 19);
+	this.color = color(255, 255, 0,80);
 	this.display = function(xpos, ypos) {
-		fill(this.color);
 		noStroke();
 		ellipseMode(CENTER);
-		ellipse(xpos, ypos, width / 12, width / 12);
-	}
-	this.move = function(step) {
-		switch (step) {
-			case 0:
-				this.display(sun_orbit_stops_xpos[0], sun_orbit_stops_ypos[0]);
-				break;
-			case 1:
-				this.display(sun_orbit_stops_xpos[1], sun_orbit_stops_ypos[1]);
-				break;
-			case 2:
-				this.display(sun_orbit_stops_xpos[2], sun_orbit_stops_ypos[2]);
-				break;
-			case 3:
-				this.display(sun_orbit_stops_xpos[3], sun_orbit_stops_ypos[3]);
-				break;
-			case 4:
-				this.display(sun_orbit_stops_xpos[4], sun_orbit_stops_ypos[4]);
-				break;
-			case 5:
-				this.display(sun_orbit_stops_xpos[5], sun_orbit_stops_ypos[5]);
-				break;
+		for(var i=0;i<50;i++){
+			fill(255,255,50,80-i);
+			ellipse(xpos, ypos, width / 15+i, width / 15+i);
 		}
+
 	}
+	this.move = function() {
+
+		}
 }
 
 function Moon() {
 	this.color = color(254, 252, 215);
 	this.display = function(xpos, ypos) {
-		fill(this.color);
 		noStroke();
 		ellipseMode(CENTER);
-		ellipse(xpos, ypos, width / 12, width / 12);
-	}
+		for(var i=0;i<50;i++){
+			fill(254,252,215,80-i);
+			ellipse(xpos, ypos, width / 15+i, width / 15+i);
+		}
+		}
 	this.move = function(step) {
 		switch (step) {
 			case 0:
@@ -238,54 +369,8 @@ function Moon() {
 	}
 }
 
-function Iceplate(start_x, start_y, str) {
-	this.color = color(255);
-	this.r = 255;
-	this.g = 255;
-	this.b = 255;
-	this.x_pt1 = start_x;
-	this.y_pt1 = start_y;
-	this.x_pt2 = start_x + width / 6;
-	this.y_pt2 = start_y;
-	if (str == "top") {
-		this.x_pt3 = map(this.x_pt2, plate_boundary[0], plate_boundary[2], plate_boundary[4], plate_boundary[6]);
-		this.x_pt4 = map(this.x_pt1, plate_boundary[0], plate_boundary[2], plate_boundary[4], plate_boundary[6]);
-	} else if (str == "bottom") {
-		this.x_pt3 = map(this.x_pt2, plate_boundary[4], plate_boundary[6], plate_boundary[8], plate_boundary[10]);
-		this.x_pt4 = map(this.x_pt1, plate_boundary[4], plate_boundary[6], plate_boundary[8], plate_boundary[10]);
-	}
-	this.y_pt3 = start_y + height / 6;
-	this.y_pt4 = this.y_pt3;
-
-	this.display = function() {
-		stroke(200);
-		fill(this.color);
-		quad(this.x_pt1, this.y_pt1, this.x_pt2, this.y_pt2, this.x_pt3, this.y_pt3, this.x_pt4, this.y_pt4);
-	}
-
-	this.height = function() {
-		var hgt = this.y_pt3 - this.y_pt2;
-		return hgt;
-	}
-	this.length = function() {
-		var length = this.x_pt2 - this.x_pt1;
-		return length;
-	}
-	this.melt = function() {
-		// -TODO- fill with decreasing opacity
-		this.r = 0;
-		this.g = 105;
-		this.b = 148;
-		this.color = color(this.r, this.g, this.b);
-		this.display();
-	}
-	this.checkColor = function() {
-		var rgbs = this.r + this.g + this.b;
-		return rgbs;
-	}
-}
-
 function Polarbear() {
+	live = true;
 
 	this.draw = function(x, y) { //TODO fix with variables
 		ellipseMode(CENTER);
@@ -321,13 +406,60 @@ function Polarbear() {
 		// ellipse(width/2+45,height/3+200,50,50);
 		arc(100 / 2 + 45 / 2 + x, 175 / 2 + 70 + y, 25, 25, radians(160), radians(20), CHORD);
 	}
+	this.drawDying = function(x,y){
+		ellipseMode(CENTER);
+		//ears
+		fill(230);
+		noStroke();
+		ellipse(100 / 2 - 30 / 2 + x, 175 / 3 - 30 / 2 + y+120, 50 / 2, 50 / 2);
+		ellipse(100 / 2 + 30 / 2 + x, 175 / 3 - 30 / 2 + y+120, 50 / 2, 50 / 2);
+		fill(150);
+		ellipse(100 / 2 - 30 / 2 + x, 175 / 3 - 30 / 2 + y+120, 38 / 2, 38 / 2);
+		ellipse(100 / 2 + 30 / 2 + x, 175 / 3 - 30 / 2 + y+120, 38 / 2, 38 / 2);
+		//face
+		noStroke();
+		fill(230);
+		ellipse(100 / 2 + x, 175 / 3 + y+120, 100 / 2, 100 / 2);
+		//eyes
+		fill(0);
+		ellipse(100 / 2 - 10 + x, 175 / 3 - 15 / 2 + y+120, 10 / 2, 10 / 2);
+		ellipse(100 / 2 + 10 + x, 175 / 3 - 15 / 2 + y+120, 10 / 2, 10 / 2);
+		//nose
+		fill(0);
+		ellipse(100 / 2 + x, 175 / 3 + 15 / 2 + y+120, 25 / 2, 20 / 2);
+	}
 
 	this.checkBound = function(){
 		if(bear_pos_x<0)
 		bear_pos_x+=10;
-		if(bear_pos_x>width)
+		if(bear_pos_x>width-50)
 		bear_pos_x-=10;
 		if(bear_pos_y>height-160)
 		bear_pos_y-=10;
 		}
+
+	this.checkDead = function(sea_level,hole_x,hole_y,hole_r,health){
+			if(bear_pos_y < sea_level-160){
+				live = false;
+			}
+			if(bear_pos_x+220<hole_x+hole_r&&bear_pos_y+160>hole_y-hole_r/4&&bear_pos_y+160 < hole_y+hole_r/4&&bear_pos_x-180>hole_x-hole_r)live=false;
+			if(health<1)live=false;
+		}
+
+	this.health = 100;
+	this.updateHealth = function(eating){
+		var prev_sec = current_sec;
+		current_sec = second();
+		if(prev_sec!=current_sec)this.health-=1;
+		if(eating)this.health+=10;
+
+	}
+	this.showHealth = function(){
+		strokeWeight(10);
+		rect(0,0,width,50);
+		noStroke();
+		fill(255,0,0);
+		currentHealth = map(this.health,1,100,0,width);
+		rect(0,0,currentHealth,50);
+	}
 }
